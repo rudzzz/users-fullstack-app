e2b7bafb7bd67b00236c098985a433c4f35702f7
rudzzz <rudsolza13@gmail.com>
2023-12-15 10:49:36 -0300

    post data into database

--- Frontend/src/components/Form.tsx
+++ Frontend/src/components/Form.tsx
@@ -1,25 +1,63 @@
-import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Form = () => {
+    let navigate = useNavigate();
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+    });
+
+    const {name, email} = user;
+
+    const handleInputChange = (event) => {
+        setUser(
+            {...user,[event.target.name]:event.target.value}
+        );
+    }
+
+    const handleFormSubmit = async(event) => {
+        event.preventDefault();
+        console.log(event);
+        await axios.post("http://localhost:8080/users", user);
+        navigate("/");
+    }
+
     return <>
         <div className="container">
             <h1>Register User</h1>
-            <form className="form" action="">
+            <form className="form" onSubmit={(event) => handleFormSubmit(event)}>
                 <div className="form-group">
                     <label htmlFor="name">Name</label>
-                    <input type="text" id="name"/>
+                    <input value={name} 
+                        type="text" 
+                        id="name"
+                        name="name"
+                        required
+                        onChange={(event) => handleInputChange(event)}
+                    />
                 </div>
 
                 <div className="form-group">
-                    <label htmlFor="name">Email</label>
-                    <input type="text" id="name"/>
+                    <label htmlFor="email">Email</label>
+                    <input value={email} 
+                        type="email" 
+                        id="email"
+                        name="email"
+                        required
+                        onChange={(event) => handleInputChange(event)}
+                    />
                 </div>
 
                 <div className="formBtn">
                     <button type="button" className="cancel-btn">
                         <Link className="navLink" to={'/'}>Cancel</Link>
                     </button>
-                    <button className="save-btn">Save</button>
+                    <button className="save-btn">
+                        Save
+                    </button>
                 </div>
 
             </form>
