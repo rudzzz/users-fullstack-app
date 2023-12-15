a8625597e75fa3aa5040d062a7ee6e732f89c6e1
rudzzz <rudsolza13@gmail.com>
2023-12-15 11:32:13 -0300

    get user by id

--- Backend/src/main/java/dev/rudzzz/user/controllers/UserController.java
+++ Backend/src/main/java/dev/rudzzz/user/controllers/UserController.java
@@ -3,9 +3,12 @@ package dev.rudzzz.user.controllers;
 import dev.rudzzz.user.entities.User;
 import dev.rudzzz.user.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -22,5 +25,16 @@ public class UserController {
     @PostMapping
     public User save(@RequestBody User user){
         return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getById(@PathVariable Long id){
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isPresent()){
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
     }
 }
