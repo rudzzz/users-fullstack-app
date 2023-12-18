e0bc764f697365db857a82af270a001807d17368
rudzzz <rudsolza13@gmail.com>
2023-12-18 11:26:23 -0300

    new attributes added

--- Backend/src/main/java/dev/rudzzz/user/entities/User.java
+++ Backend/src/main/java/dev/rudzzz/user/entities/User.java
@@ -3,6 +3,8 @@ package dev.rudzzz.user.entities;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 
+import java.util.Date;
+
 @Entity
 @Table(name = "users")
 public class User {
@@ -12,6 +14,9 @@ public class User {
     private String name;
     @Email
     private String email;
+    private Date dateOfBirth;
+    private Integer phoneNumber;
+    private String gender;
 
     public User() {
 
@@ -39,5 +44,29 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 }
