b5246a2c97c78456b8702d890a5ffac48b36d693
rudzzz <rudsolza13@gmail.com>
2023-12-18 11:49:28 -0300

    refactoring to use the usable functions file

--- Frontend/src/components/Form.tsx
+++ Frontend/src/components/Form.tsx
@@ -120,9 +120,9 @@ const Form = () => {
                         className="formSelect"
                         onChange={(event) => handleInputChange(event)}
                     >
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="other">Other</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="Other">Other</option>
                     </select>
                 </div>
 
--- Frontend/src/components/List.tsx
+++ Frontend/src/components/List.tsx
@@ -1,39 +1,50 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { fetchUserData, formatDate } from "../functions";
 
 const List = () => {
     const {id}  = useParams();
     const [userData, setUserData] = useState(null);
 
     useEffect( () => {
-        const fetchUserData = async () => {
-            try{
-                const response = await axios.get(`http://localhost:8080/users/${id}`);
-                setUserData(response.data);
-                console.log(response.data)
-            } catch (error){
-                alert(`Error fetching user data: ${error}`);
-            }
-        };
-
-        fetchUserData();
-        
+        const fetchData = async () => {
+            const data = await fetchUserData(id);
+            setUserData(data);
+        }
+        fetchData();        
     }, [id]);
 
     if(!userData){
         return null;
     }
 
-    const {name, email} = userData;
+    const { name, email, dateOfBirth, phoneNumber, gender } = userData;
 
     return <>
         <div className="container">
             <h1 className="detailH1">Details</h1>
             <div className="userDetail">
                 <ul>
-                    <li><b>Name:</b> {name}</li>
-                    <li><b>Email:</b> {email}</li>
+                    <li>
+                        <b>Name:</b>
+                        {name}
+                    </li>
+                    <li>
+                        <b>Email:</b>
+                        {email}
+                    </li>
+                    <li>
+                        <b>Date of Birth:</b>
+                        {formatDate(dateOfBirth)}
+                    </li>
+                    <li>
+                        <b>Phone Number:</b>
+                        {phoneNumber}
+                    </li>
+                    <li>
+                        <b>Gender:</b>
+                        {gender}
+                    </li>
                 </ul>
             </div>
             <Link className="home-btn linkBtn navLink" to={'/'}>Back to Home</Link>
